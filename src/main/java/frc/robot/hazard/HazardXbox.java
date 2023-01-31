1c8399564f10abb19b14aca31fd121597bd61113
Carson Seese <carson@carsonseese.com>
2023-01-31 16:41:43 -0500

    Add rumble support and better comments

--- src/main/java/frc/robot/hazard/HazardXbox.java
+++ src/main/java/frc/robot/hazard/HazardXbox.java
@@ -1,25 +1,66 @@
-package frc.robot.util;
+package frc.robot.hazard;
 
+import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-
-enum Side {
-  LEFT,
-  RIGHT
-}
 
 public class HazardXbox extends CommandXboxController {
 
   private float deadzone = 0;
+  private XboxController hid;
 
-  public HazardXbox(int port) {
-    super(port);
-  }
-
+  /**
+   * Initializes a new Xbox controller with the specified port and deadzone
+   *
+   * @param port USB port of the controller
+   * @param deadzone Deadzone (0-1
+   */
   public HazardXbox(int port, float deadzone) {
-    super(port);
+    this(port);
 
     this.deadzone = deadzone;
   }
+
+  /**
+   * Initializes a new Xbox controller with the specified port
+   *
+   * @param port USB port of the controller
+   */
+  public HazardXbox(int port) {
+    super(port);
+
+    this.hid = super.getHID();
+  }
+
+  /**
+   * Rumble the controller
+   *
+   * @param amount Value to rumble (0 - 1)
+   */
+  public void rumble(float amount) {
+    this.rumbleLeft(amount);
+    this.rumbleRight(amount);
+  }
+
+  /**
+   * Rumble the left side of the controller
+   *
+   * @param amount Value to rumble (0 - 1)
+   */
+  public void rumbleLeft(float amount) {
+    this.hid.setRumble(GenericHID.RumbleType.kLeftRumble, amount);
+  }
+
+  /**
+   * Rumble the right side of the controller
+   *
+   * @param amount Value to rumble (0 - 1)
+   */
+  public void rumbleRight(float amount) {
+    this.hid.setRumble(GenericHID.RumbleType.kRightRumble, amount);
+  }
+
+  /* The below code consists of overrides and new methods to apply deadzone */
 
   /**
    * Returns the value of the axis after accounting for the configured deadzone
