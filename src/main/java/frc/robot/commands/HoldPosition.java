8cf28b4714d27329dd7ab345cf68992cee323dd2
Kiefer <agentaston967st@gmail.com>
2023-03-16 20:22:25 -0400

    Minor changed to HoldPosition

--- src/main/java/frc/robot/commands/HoldPosition.java
+++ src/main/java/frc/robot/commands/HoldPosition.java
@@ -6,13 +6,18 @@ import edu.wpi.first.math.numbers.N1;
 import edu.wpi.first.math.numbers.N3;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.Subsystem;
 import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.InformationSubsystem;
+import java.util.HashSet;
+import java.util.Set;
 
 public class HoldPosition extends CommandBase {
   private DriveSubsystem drive;
   private InformationSubsystem info;
   private Matrix<N3, N1> targetPose;
+
+  private Set<Subsystem> requirements;
 
   private PIDController X;
   private PIDController Y;
@@ -21,6 +26,9 @@ public class HoldPosition extends CommandBase {
   public HoldPosition(DriveSubsystem drive, Matrix<N3, N1> target) {
     this.drive = drive;
     targetPose = target;
+
+    requirements = new HashSet<Subsystem>();
+    requirements.add(drive);
 
     X = new PIDController(0.2, 0, 0);
     Y = new PIDController(0.2, 0, 0);
@@ -40,5 +48,10 @@ public class HoldPosition extends CommandBase {
           Y.calculate(info.getPoseEstimate().get(1, 0), targetPose.get(1, 0)),
           Z.calculate(info.getPoseEstimate().get(2, 0), targetPose.get(2, 0))
         });
+  }
+
+  @Override
+  public Set<Subsystem> getRequirements() {
+    return requirements;
   }
 }
