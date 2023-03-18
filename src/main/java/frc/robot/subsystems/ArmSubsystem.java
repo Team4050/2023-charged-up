4775d14760cabc8add2f76d3d5fcc73663e17a45
Kiefer <agentaston967st@gmail.com>
2023-03-18 15:03:37 -0400

    Added arm position constants

--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -9,8 +9,7 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class ArmSubsystem extends SubsystemBase {
-  // TODO: Piston variable name should be more descriptive when we know what it does
-  private final DoubleSolenoid piston =
+  private final DoubleSolenoid clawAlignmentPiston =
       new DoubleSolenoid(
           Constants.Pneumatics.PCM,
           Constants.Pneumatics.Module,
@@ -26,14 +25,10 @@ public class ArmSubsystem extends SubsystemBase {
   public ArmSubsystem() {}
 
   @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
-  }
+  public void periodic() {}
 
   @Override
-  public void simulationPeriodic() {
-    // This method will be called once per scheduler run during simulation
-  }
+  public void simulationPeriodic() {}
 
   /** Test method, please ignore */
   public void test() {
