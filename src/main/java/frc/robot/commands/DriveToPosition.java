d7c3767dbe7a1c67c669be30a348aed57af0fc02
Kiefer <agentaston967st@gmail.com>
2023-04-01 11:34:45 -0400

    Transition to using HolonomicDriveController

--- src/main/java/frc/robot/commands/DriveToPosition.java
+++ src/main/java/frc/robot/commands/DriveToPosition.java
@@ -1,10 +1,14 @@
 package frc.robot.commands;
 
+import edu.wpi.first.math.controller.HolonomicDriveController;
 import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.trajectory.Trajectory;
 import edu.wpi.first.math.trajectory.TrajectoryConfig;
 import edu.wpi.first.math.trajectory.TrajectoryGenerator;
+import edu.wpi.first.math.trajectory.TrapezoidProfile.Constraints;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.DriveSubsystem;
@@ -14,46 +18,36 @@ import java.util.ArrayList;
 public class DriveToPosition extends CommandBase {
   private DriveSubsystem drive;
   private InformationSubsystem info;
-  private Pose2d targetPose;
   private Trajectory trajectory;
   private PIDController X;
   private PIDController Y;
-  private PIDController Rotation;
+  private ProfiledPIDController Rotation;
+  private HolonomicDriveController controller;
   private Timer timer;
 
   public DriveToPosition(DriveSubsystem drive, InformationSubsystem info, ArrayList<Pose2d> curve) {
     this.drive = drive;
     this.info = info;
-    targetPose = curve.get(curve.size() - 1);
     trajectory = TrajectoryGenerator.generateTrajectory(curve, new TrajectoryConfig(10, 2));
   }
 
   @Override
   public void execute() {
+    ChassisSpeeds speeds =
+        controller.calculate(
+            info.getPoseEstimate().toPose2d(),
+            trajectory.sample(timer.get()),
+            trajectory.sample(timer.get()).poseMeters.getRotation());
     drive.driveFieldRelative(
-        X.calculate(
-            info.getPoseEstimate().getX() - trajectory.sample(timer.get()).poseMeters.getX()),
-        Y.calculate(
-            info.getPoseEstimate().getY() - trajectory.sample(timer.get()).poseMeters.getY()),
-        /* Very complicated function string, basically just take the current robot rotation,
-         * rotate that by the inverse of the target rotation, and measure that rotation's degree to get the error value.
-         */
-        Rotation.calculate(
-            info.getPoseEstimate()
-                .toPose2d()
-                .getRotation()
-                .rotateBy(trajectory.sample(timer.get()).poseMeters.getRotation().unaryMinus())
-                .getRadians()));
+        speeds.vxMetersPerSecond, speeds.vyMetersPerSecond, speeds.omegaRadiansPerSecond);
   }
 
   @Override
   public void initialize() {
     X = new PIDController(0.3, 0.1, 0.2);
     Y = new PIDController(0.3, 0.1, 0.2);
-    Rotation = new PIDController(0.3, 0.1, 0.2);
-    X.setSetpoint(0);
-    Y.setSetpoint(0);
-    Rotation.setSetpoint(0);
+    Rotation = new ProfiledPIDController(0.3, 0.1, 0.2, new Constraints(1, 1));
+    controller = new HolonomicDriveController(X, Y, Rotation);
     timer = new Timer();
     timer.start();
   }
