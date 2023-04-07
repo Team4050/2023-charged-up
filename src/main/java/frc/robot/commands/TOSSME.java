cbc2776829093a0f0241439291401cbe0637620a
Kiefer <agentaston967st@gmail.com>
2023-04-07 14:33:08 -0400

    Toss command

---
+++ src/main/java/frc/robot/commands/TOSSME.java
@@ -0,0 +1,55 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.Subsystem;
+import frc.robot.subsystems.ArmSubsystem;
+import frc.robot.subsystems.ClawSubsystem;
+import java.util.HashSet;
+import java.util.Set;
+
+public class TOSSME extends CommandBase {
+  private final double grabTiming = 0.05; // 50ms after wrist flick
+  private final double end = 0.2; // 200ms timeout
+  private Timer timer;
+  private ClawSubsystem claw;
+  private ArmSubsystem arm;
+  private Set<Subsystem> reqs;
+
+  public TOSSME(ArmSubsystem arm, ClawSubsystem claw) {
+    this.arm = arm;
+    this.claw = claw;
+    reqs = new HashSet<Subsystem>();
+    reqs.add(arm);
+    reqs.add(claw);
+  }
+
+  @Override
+  public void initialize() {
+    timer = new Timer();
+    timer.reset();
+    timer.start();
+  }
+
+  @Override
+  public void execute() {
+    if (timer.get() > grabTiming) {
+      claw.setTargetState(Value.kReverse);
+    } else {
+      claw.setTargetState(Value.kForward);
+    }
+    claw.activate();
+    arm.setClawAlignment(true);
+  }
+
+  @Override
+  public boolean isFinished() {
+    return timer.get() > end;
+  }
+
+  @Override
+  public Set<Subsystem> getRequirements() {
+    return reqs;
+  }
+}
